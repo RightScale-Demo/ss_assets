0b96d4c9a68d0e74ce0f8f4e20f4a34cb8f195ef
MitchellGerdisch <mitchell.gerdisch@rightscale.com>
2018-03-27 18:44:38 -0500

    Updated

--- Travis_CATs/RL10LampStack.cat.rb
+++ Travis_CATs/RL10LampStack.cat.rb
@@ -22,7 +22,7 @@
 # User can use a post-launch action to install a different version of the application software from a Git repo.
 
 
-name 'Engineering LAMP Stack'
+name 'Engineering LAMP Stack Alpha'
 rs_ca_ver 20160622
 short_description "![logo](https://s3.amazonaws.com/rs-pft/cat-logos/lamp_icon.png)
 
